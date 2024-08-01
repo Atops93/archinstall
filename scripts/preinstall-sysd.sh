ad5e79ad327e0e15cb4fe862c6ff3bb4091ca6dd
atops <aetops93@proton.me>
2024-08-01 16:45:43 +0930

    aaf

--- scripts/preinstall-sysd.sh
+++ scripts/preinstall-sysd.sh
@@ -12,10 +12,6 @@ read SWAP
 echo "Enter EFI partition: "
 read EFI
 
-echo "Choose a Desktop Environment"
-echo "1. None"
-echo "2. KDE"
-
 # format
 mkfs.ext4 -L "ROOT" "${ROOT}"
 mkswap "${SWAP}"
@@ -27,9 +23,8 @@ swapon "${SWAP}"
 mount -t fat "${EFI}" /mnt/boot
 
 # mirrors
-curl https://archlinux.org/mirrorlist/?country=AU&protocol=http&protocol=https&ip_version=4
+lynx https://archlinux.org/mirrorlist/?country=AU&protocol=http&protocol=https&ip_version=4
 
-# packages
 pacstrap -K /mnt base linux linux-firmware intel-ucode networkmanager sof-firmware nano
 
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -37,6 +32,14 @@ arch-chroot /mnt
 sed -i 's/^#en_AU.UTF-8 UTF-8/en_AU.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_AU.UTF-8" >> /etc/locale.conf
+
+
+if ! [ $hostname ]
+then
+  hostname="arch"
+fi
+
+echo $hostname >> /etc/hostname
 
 passwd
 
@@ -58,6 +61,11 @@ options root=${ROOT} rw
 EOF
 
 systemctl enable NetworkManager
+
+echo "Choose a Desktop Environment"
+echo "1. None"
+echo "2. KDE"
+
 exit
 umount -R /mnt
 
