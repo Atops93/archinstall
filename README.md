10b72e51ebf345a0a72320c1baf82ae19f7a7e74
Sir Atops <aetops93@proton.me>
2024-10-26 17:30:04 +1030

    need to update install.sh but works for now.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# arch-script
- Atops arch install script. Warning! This script was only made for me not you.
+ Atops custom archinstall script to make life easy when installing arch. Warning!! This script was intended for me to use, not you. Of course you can try it if you want.
 
 # (STILL IN PROGRESS.)
 
- ```curl https://raw.githubusercontent.com/atops93/archinstall/main/autoinstall.sh -o autoinstall.sh && sh autoinstall.sh```
+ ```curl https://raw.githubusercontent.com/atops93/archinstall/main/install.sh -o install.sh && sh install.sh```
+
+### (SHORTER TYPING METHOD) 
+```curl -LO https://atops.dev/install.sh && sh install.sh```
--- ato-post-setup.sh
+++ ato-post-setup.sh
@@ -1,50 +1,83 @@
-echo "Make sure to connect to the internet on setup again so pkgs can be installed."
-echo "(1) I'm Connected to the Net bozo"
-echo "(2) Exit by ctrl + c or ctrl + D & connect to wifi then open script again."
+#!/bin/bash
 
-###############################
-#	  TESTING CONNECTION	  #
-###############################
-ping archlinux.org
+echo "Make sure you are connected to the internet so packages can be installed."
+echo "(1) I'm Connected to the Net!"
+echo "(2) Exit with Ctrl+C or Ctrl+D, connect to WiFi, and rerun this script."
 
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd
-yay -S firefox wget curl lynx gcc repo rsync glava thunar gvfs picom openssh flameshot alacritty btop htop qemu-full edk2-ovmf rofi dxvk-bin cava cmatrix-git jellyfin nginx cmus dosfstools mtools cmake pipewire lib32-pipewire wireplumber fastfetch meson musl p7zip unzip 
+# Testing internet connection
+echo "Testing internet connection..."
+if ! ping -c 1 archlinux.org &>/dev/null; then
+    echo "No internet connection. Please connect and try again."
+    exit 1
+else
+    echo "Internet connection verified."
+fi
 
-#sudo echo "[chaotic-aur]" >> /etc/pacman.conf
-#sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+# Install yay (AUR helper)
+if ! command -v yay &>/dev/null; then
+    echo "Installing yay..."
+    git clone https://aur.archlinux.org/yay.git
+    cd yay || exit
+    makepkg -si --noconfirm
+    cd ..
+else
+    echo "yay is already installed."
+fi
 
-##################################################################
-####  I DID SAY AT THE START THIS IS MY METHOD OF SETTING UP  ####
-##################################################################
+# Install essential packages
+echo "Installing packages..."
+yay -S --noconfirm firefox wget curl lynx gcc rsync glava thunar gvfs picom \
+    openssh flameshot alacritty btop qemu-full edk2-ovmf rofi dxvk-bin \
+    cava cmatrix-git jellyfin cmus dosfstools mtools cmake pipewire \
+    lib32-pipewire wireplumber fastfetch meson musl p7zip unzip
 
-echo "(1) Clone my dwm & use alacritty?" READ dwm
-echo "(2) Install kde without display manager & use startx instead?"
-echo "(3) Clone "
-echo "(4) Clone "
-echo "(5) Clone "
+# Optional Chaotic-AUR setup (commented out)
+# echo "Setting up Chaotic-AUR repository..."
+# sudo tee -a /etc/pacman.conf <<EOL
+# [chaotic-aur]
+# Include = /etc/pacman.d/chaotic-mirrorlist
+# EOL
+# sudo pacman -Sy
 
+# User prompt for environment setup
+echo "Choose an environment setup option:"
+echo "(1) Clone and install dwm setup with Alacritty"
+echo "(2) Install KDE (no display manager, use startx)"
+echo "(3) Placeholder for custom environment"
+echo "(4) Placeholder for custom environment"
+echo "(5) Placeholder for custom environment"
+read -rp "Enter your choice [1-5]: " choice
 
+case $choice in
+    1)
+        echo "Cloning and setting up dwm..."
+        git clone https://github.com/atops93/ato-dwm
+        cd ato-dwm || exit
+        chmod +x install.sh
+        ./install.sh
+        cd ..
+        ;;
+    2)
+        echo "Installing KDE..."
+        sudo pacman -S --noconfirm plasma-desktop xorg-server xorg-xinit
+        ;;
+    3)
+        echo "Custom environment setup placeholder 1"
+        # Add any commands for custom environment setup here
+        ;;
+    4)
+        echo "Custom environment setup placeholder 2"
+        # Add any commands for custom environment setup here
+        ;;
+    5)
+        echo "Custom environment setup placeholder 3"
+        # Add any commands for custom environment setup here
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
 
+echo "Setup complete! Reboot or restart your display manager as needed."
 
-
-
-
-
-
-
-if [[1]] ; then
-	sudo pacman -S xorg xorg-server xorg-xinit && git clone https://github.com/atops93/ato-dwm && cd ato-dwm && chmod +x install.sh && ./install.sh
-if [[2]] ; then
-	sudo pacman -S plasma-desktop && 
-if [[3]] ; then
-
-
-
-
-
-	echo "THE END"
-	rm -rf ato-post-setup.sh
-	exit
